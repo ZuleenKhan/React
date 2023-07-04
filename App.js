585707bea81776a3c817ee7b1e8bb84358310409
ZuleenKhan <85193495+ZuleenKhan@users.noreply.github.com>
2023-07-04 13:22:22 +0530

    Second Commit Made

---
+++ App.js
@@ -0,0 +1,7 @@
+
+const heading = React.createElement("h1",{} , "Zuleen Taker") ; 
+
+
+const root = ReactDOM.createRoot(document.getElementById("root")) ; 
+     // passing a react element inside the root  
+     root.render(heading) ; 
