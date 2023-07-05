469920d962309c4a3640c2f0970c35ae755b38f4
ZuleenKhan <85193495+ZuleenKhan@users.noreply.github.com>
2023-07-06 01:10:02 +0530

    third commit

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from "react" ; 
+import ReactDOM from "react-dom/client"; 
+import App from './App.js' ; 
 
 const heading = React.createElement("h1",{} , "Zuleen Taker") ; 
-
-
+//console.log(heading) ; 
 const root = ReactDOM.createRoot(document.getElementById("root")) ; 
-     // passing a react element inside the root  
-     root.render(heading) ; 
+// passing a react element inside the root  
+root.render(heading) ; 
